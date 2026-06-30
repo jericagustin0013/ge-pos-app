2ec62ce719b46dd5721e99e3ece910859cfe81d5
Jeric Agustin <jericagustin0013@gmail.com>
2026-06-30 14:55:11 +0800

    Initial commit.

---
+++ sw.js
@@ -0,0 +1,56 @@
+const CACHE_NAME = "printdesk-pos-v4";
+const APP_SHELL = [
+  "./",
+  "./printing-pos.html",
+  "./android-install.html",
+  "./manifest.webmanifest",
+  "./printdesk-icon.svg",
+  "./printdesk-icon-192.png",
+  "./printdesk-icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put("./printing-pos.html", copy));
+          return response;
+        })
+        .catch(() => caches.match("./printing-pos.html"))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      const fresh = fetch(event.request).then(response => {
+        if (response && response.ok) {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        }
+        return response;
+      }).catch(() => cached);
+      return cached || fresh;
+    })
+  );
+});
